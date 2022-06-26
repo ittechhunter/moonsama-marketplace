b06d4e57b629c13149e2da32dde0d003b15beddc
kyilkhor <kyilkhorsama@gmail.com>
2022-06-26 13:13:57 +0200

    feat: add sunken scroll

--- src/assets/data/collections.ts
+++ src/assets/data/collections.ts
@@ -282,7 +282,7 @@ export const collections = {
       floorDisplay: false,
       symbol: 'MEMBASSY',
       minId: 1,
-      maxId: 13,
+      maxId: 15,
       type: 'ERC1155',
       contractURI: 'ipfs://QmWzBDhV5nuPrud7XZo2vBLBWtTStwc6N8xSwCzuMdewd8',
       subgraph: 'https://moonriver-subgraph.moonsama.com/subgraphs/name/moonsama/nft-1155-embassy'
