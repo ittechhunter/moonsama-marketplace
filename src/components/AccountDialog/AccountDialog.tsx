6cc4c073ca82363b3f9a8c9e80447cd9dca3cdba
Tony <neoprince1992@gmail.com>
2022-07-01 21:27:07 +0900

    feat: try for mobile version

--- src/components/AccountDialog/AccountDialog.tsx
+++ src/components/AccountDialog/AccountDialog.tsx
@@ -319,7 +319,7 @@ export const AccountDialog = () => {
 
   function getModalContent() {
     console.log("121312321", { isMobile, error, connector, walletView, account})
-    if (error) {
+    if (error || isMobile) {
       return (
         <div className={styles.dialogContainer}>
           {(error instanceof UnsupportedChainIdError || isMobile) && <>
@@ -347,7 +347,7 @@ export const AccountDialog = () => {
             </Button>
           </>}
 
-          {!(error instanceof UnsupportedChainIdError) && <>
+          {!(error instanceof UnsupportedChainIdError || isMobile) && <>
             <div>
               Something went wrong
             </div>
