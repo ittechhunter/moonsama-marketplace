b4de3c21e55230d9ed10db582b82934b4490a632
Tony <neoprince1992@gmail.com>
2022-07-05 09:03:55 +0900

    feat: change marketplace Subgraph URL

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -44,7 +44,9 @@ export const NETWORK_NAME: { [chainId: number]: string } = {
 };
 
 export const MARKETPLACE_SUBGRAPH_URLS: { [chainId: number]: string } = {
-  [ChainId.MOONRIVER]: 'https://moonriver-subgraph.moonsama.com/subgraphs/name/moonsama/marketplacev5', // latest multicall 2 deployments
+
+  [ChainId.MOONRIVER]: 'https://moonriver-subgraph.moonsama.com/subgraphs/name/moonsama/marketplacev6',
+  // [ChainId.MOONRIVER]: 'https://moonriver-subgraph.moonsama.com/subgraphs/name/moonsama/marketplacev5', // latest multicall 2 deployments
   [ChainId.MOONBEAM]: 'https://moonbeam-subgraph.moonsama.com/subgraphs/name/moonsama/marketplacev4'
 };
 
